0acac860e858dad16b6bbeeb4b487f20865d9cfc
suica1010 <63956549+suica1010@users.noreply.github.com>
2025-03-26 09:40:24 +0900

    quot fix

--- js_practice/test.js
+++ js_practice/test.js
@@ -1,15 +1,16 @@
 console.log(document.querySelector('#title'))
 const h1 = document.querySelector('#title')
 let age = 30
-let name = "jason"
+let firstname = "jason"
 let content = "裁縫"
 
-const hello = (name, age, content) => 
-    "「こんにちは、私は${age}歳の${name}です。趣味は${content}です。」"
+//jsの場合、テンプレートリテラルを使用する場合`(バックオートで囲む)
+const hello = (firstname, age, content) => 
+    `「こんにちは、私は${age}歳の${firstname}です。趣味は${content}です。」`
 
 //要素が存在する場合
 if(h1){
-    h1.innerHTML = hello(name, age, content)
+    h1.innerHTML = hello(firstname, age, content)
 }else{
     //要素が見つからない場合
     console.log("要素#titleが見つかりません")
